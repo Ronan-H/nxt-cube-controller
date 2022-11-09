e52e4df9c3095f28d1b3dee0dd2800adb8dddea8
Ronan Hanley <ronan.h64@gmail.com>
2022-11-09 20:58:06 +0000

    Make turn degrees variable

--- cube_controller.py
+++ cube_controller.py
@@ -9,14 +9,21 @@ from pyjoystick.sdl2 import Key, run_event_loop
 import queue
 import threading
 
+claw_motor_port = nxt.motor.Port.A
+table_motor_port = nxt.motor.Port.B
+
+quarter_turn_degrees = 270
+turn_limit_degrees = quarter_turn_degrees * 3
+full_turn_degrees = quarter_turn_degrees * 4
+
 action_queue = queue.Queue()
 
 
 class RobotAction(Enum):
-    CLEAR_ACTION_QUEUE, EXIT, LEFT_ROT, RIGHT_ROT, CLAW_HOLD, CLAW_FLIP = range(6)
+    CLEAR_ACTION_QUEUE, EXIT, LEFT_ROT, RIGHT_ROT, CLAW_HOLD, CLAW_UNHOLD, CLAW_FLIP = range(7)
 
 
-ps3_button_mapping = {
+ps3_button_down_mapping = {
     1: RobotAction.CLEAR_ACTION_QUEUE,
     2: RobotAction.EXIT,
 
@@ -30,6 +37,11 @@ ps3_button_mapping = {
     13: RobotAction.CLAW_FLIP,
     14: RobotAction.CLAW_FLIP,
     15: RobotAction.CLAW_FLIP,
+}
+
+ps3_button_up_mapping = {
+    10: RobotAction.CLAW_UNHOLD,
+    11: RobotAction.CLAW_UNHOLD,
 }
 
 
@@ -51,27 +63,29 @@ class Nxt:
         return b
 
     def wait_for_motors(self):
-        motors = (nxt.motor.Port.A, nxt.motor.Port.B, nxt.motor.Port.C)
+        motors = (claw_motor_port, table_motor_port)
         while not all(self.mc.is_ready(motor) for motor in motors):
             sleep(0.1)
 
     def rotate_motor(self, port: nxt.motor.Port, degrees):
         relative_power = self.motor_power if degrees > 0 else -self.motor_power
         self.mc.cmd(port, relative_power, abs(degrees), smoothstart=False, brake=True)
+        self.wait_for_motors()
 
     def stop(self):
         self.mc.stop()
 
 
 class RobotController:
-    def __init__(self, motor_power, claw_hold_rotation, claw_flip_rotation):
+    def __init__(self, motor_power, claw_hold_rotation, claw_full_flip_rotation):
         self.table_rotation = 0 # degrees, in the range 0 - 270 (360 would be represented as 0)
         self.is_claw_holding = False
         self.claw_hold_rotation = claw_hold_rotation
-        self.claw_flip_rotation = claw_flip_rotation
+        self.claw_full_flip_rotation = claw_full_flip_rotation
+        self.claw_hold_flip_rotation = self.claw_full_flip_rotation - self.claw_hold_rotation
         self.nxt = Nxt(motor_power)
 
-    def exec_action_and_wait(self, action: RobotAction):
+    def exec_action(self, action: RobotAction):
         print('Executing action:', action.name)
 
         if action == RobotAction.LEFT_ROT:
@@ -80,52 +94,50 @@ class RobotController:
             self.rotate_table_cw()
         elif action == RobotAction.CLAW_HOLD:
             self.exec_claw_hold_action()
+        elif action == RobotAction.CLAW_UNHOLD:
+            self.exec_claw_unhold_action()
         elif action == RobotAction.CLAW_FLIP:
             self.exec_claw_flip_action()
 
         print('New table rotation:', self.table_rotation)
 
-        self.nxt.wait_for_motors()
-
     def exec_table_rotation(self, degrees):
-        self.nxt.rotate_motor(nxt.motor.Port.A, degrees)
+        self.nxt.rotate_motor(table_motor_port, degrees)
         self.table_rotation += degrees
 
     def rotate_table_cw(self):
-        if self.table_rotation >= 270:
-            self.exec_table_rotation(-270)
+        if self.table_rotation >= turn_limit_degrees:
+            self.exec_table_rotation(-turn_limit_degrees)
         else:
-            self.exec_table_rotation(90)
+            self.exec_table_rotation(quarter_turn_degrees)
 
     def rotate_table_acw(self):
         if self.table_rotation <= 0:
-            self.exec_table_rotation(270)
+            self.exec_table_rotation(turn_limit_degrees)
         else:
-            self.exec_table_rotation(-90)
+            self.exec_table_rotation(-quarter_turn_degrees)
 
     def exec_claw_hold_action(self):
-        if self.is_claw_holding:
-            self.retract_claw()
-            self.is_claw_holding = False
-        else:
-            self.extend_claw()
+        if not self.is_claw_holding:
+            self.nxt.rotate_motor(claw_motor_port, self.claw_hold_rotation)
             self.is_claw_holding = True
 
-    def extend_claw(self):
-        self.nxt.rotate_motor(nxt.motor.Port.B, self.claw_hold_rotation)
-
-    def retract_claw(self):
-        self.nxt.rotate_motor(nxt.motor.Port.B, -self.claw_hold_rotation)
+    def exec_claw_unhold_action(self):
+        if self.is_claw_holding:
+            self.nxt.rotate_motor(claw_motor_port, -self.claw_hold_rotation)
+            self.is_claw_holding = False
 
     def exec_claw_flip_action(self):
-        self.nxt.rotate_motor(nxt.motor.Port.B, self.claw_flip_rotation)
-        self.nxt.rotate_motor(nxt.motor.Port.B, -self.claw_flip_rotation)
+        rotation_needed = self.claw_hold_flip_rotation if self.is_claw_holding else self.claw_full_flip_rotation
+
+        self.nxt.rotate_motor(claw_motor_port, rotation_needed)
+        self.nxt.rotate_motor(claw_motor_port, -rotation_needed)
 
 
 robot = RobotController(
-    motor_power=50,
-    claw_hold_rotation=75, # TODO: find the right value for this
-    claw_flip_rotation=90 # TODO: find the right value for this
+    motor_power=100,
+    claw_hold_rotation=50,  # TODO: find the right value for this
+    claw_full_flip_rotation=90  # TODO: find the right value for this
 )
 
 
@@ -143,12 +155,17 @@ def process_action(action: RobotAction):
 def init_ps3_controller_events():
     def on_input_received(key: Key):
         key_str = str(key)
-        if key.get_value() == 1 and key_str.startswith('Button '):
+        if key_str.startswith('Button '):
             button = int(key_str[7:])
             print('button:', button, 'val:', key.get_value())
-            if button in ps3_button_mapping:
-                action = ps3_button_mapping.get(button)
-                process_action(action)
+            if key.get_value() == 0:
+                if button in ps3_button_up_mapping:
+                    action = ps3_button_up_mapping.get(button)
+                    process_action(action)
+            elif key.get_value() == 1:
+                if button in ps3_button_down_mapping:
+                    action = ps3_button_down_mapping.get(button)
+                    process_action(action)
 
     run_event_loop(
         lambda c: print('Controller added:', c),
@@ -160,7 +177,7 @@ def init_ps3_controller_events():
 def action_consumer():
     while True:
         next_action = action_queue.get()
-        robot.exec_action_and_wait(next_action)
+        robot.exec_action(next_action)
 
 
 threading.Thread(target=action_consumer, daemon=True).start()
